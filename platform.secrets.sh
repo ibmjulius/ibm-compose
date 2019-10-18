6be8c320157718cf1a304e963c1f3a1bd4b991a2
DEVOPS <root@webdev01.devopsdemo.com.ph>
2019-10-18 03:26:45 -0500

    updated platform

--- platform.secrets.sh
+++ platform.secrets.sh
@@ -7,16 +7,16 @@ export INITIAL_ADMIN_USER="admin"
 export INITIAL_ADMIN_PASSWORD_PLAIN="admin"
 
 # Password for the Jenkins user
-export PASSWORD_JENKINS="e1ab09a3cdd49595"
+export PASSWORD_JENKINS="a822b6f21dc67857"
 
 # Password for the Gerrit user
-export PASSWORD_GERRIT="9ce88fffd5f77441"
+export PASSWORD_GERRIT="5cf915aa77c010828"
 
 # Root password for the Sonar and Gerrit MySQL instances
-export PASSWORD_SQL="78e91ab805879564"
+export PASSWORD_SQL="213e3e10e1881266"
 
 # Root password for Gitlab admin user
-export PASSWORD_GITLAB="0e8caa9e02764603"
+export PASSWORD_GITLAB="fe20adb75ed73172"
 
 # Root password for the LDAP server
-export LDAP_PWD="795835acbb402405" 
+export LDAP_PWD="eacf06eb96424106" 
