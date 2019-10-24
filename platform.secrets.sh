90ecebedc5b4f1eb6cdbcfe1308fa3f40840ff6f
lotus <lotus.notes@ibm.com>
2019-10-24 03:01:23 -0500

    A

--- platform.secrets.sh
+++ platform.secrets.sh
@@ -7,16 +7,16 @@ export INITIAL_ADMIN_USER="admin"
 export INITIAL_ADMIN_PASSWORD_PLAIN="admin"
 
 # Password for the Jenkins user
-export PASSWORD_JENKINS="a822b6f21dc67857"
+export PASSWORD_JENKINS="7ee030971c7c8204"
 
 # Password for the Gerrit user
-export PASSWORD_GERRIT="5cf915aa77c010828"
+export PASSWORD_GERRIT="6fecb24468b410821"
 
 # Root password for the Sonar and Gerrit MySQL instances
-export PASSWORD_SQL="213e3e10e1881266"
+export PASSWORD_SQL="b595f35e1c433776"
 
 # Root password for Gitlab admin user
-export PASSWORD_GITLAB="fe20adb75ed73172"
+export PASSWORD_GITLAB="b2709fce24d61071"
 
 # Root password for the LDAP server
-export LDAP_PWD="eacf06eb96424106" 
+export LDAP_PWD="1c218334fb186032" 
